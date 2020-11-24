3d49001e7b122179107ebef1d2fc295ab8567a2f
PARK SUNG DONG <oraedoa@gmail.com>
2020-11-24 18:15:38 +0900

    [init]

---
+++ README.md
@@ -0,0 +1,2 @@
+## springboot-batch-basis
+* 스프링부트 기반의 배치를 hello world 수준으로 do-it do-it
---
+++ src/main/java/edu/pasudo123/study/demo/SpringbootBatchBasisApplication.java
@@ -0,0 +1,13 @@
+package edu.pasudo123.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatchBasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBatchBasisApplication.class, args);
+    }
+
+}
